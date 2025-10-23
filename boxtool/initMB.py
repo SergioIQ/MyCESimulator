e1454fd753b11f38652c15e73caa0ea4faca4697
SergioIQ <sergio.personal.iq@gmail.com>
2025-10-24 00:15:18 +1300

    Update and rename StartTable to initMB.py
    Table creator

---
+++ boxtool/initMB.py
@@ -0,0 +1,16 @@
+def initMB(inps, outs, compounds):
+    flow='flow' #copy paste to avoid writing mistakes
+    """Create a dataframe with columns with the name of inputs and outputs and index with studied properties
+     inps and outs are numbers while prop is a list with strs"""
+    #Mass balance without Q and without R has C indipendent
+    #liberty freedom (C*(S-1))
+    #La fila de totales debe incluirse por lo que debo quitarle 1 adicional a los calculos de grados de libertad
+    #De momento la matriz debe mostrar los caucales masicos de cada compuesto, ya que mezclar composiciones y caudales complica la automatizacion de la resolucion
+    '''En el futuro crear una funcion que cree la analoga de composiciones'''
+    streams=[]
+    for i in range(inps):
+        streams.append('IN-'+str(i+1))
+    for i in range(outs):
+        streams.append('OU-'+str(i+1))
+    MB=pd.DataFrame(columns=streams, index=compounds)  # Fixed: changed prop to compounds based on function parameters
+    return MB
